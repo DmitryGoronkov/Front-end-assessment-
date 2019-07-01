5041eb0c7b9e4273eab354278bebe58a7105d285
Dmitry Goronkov <gordima@yahoo.com>
2019-07-01 15:01:22 -0400

    basic fetching done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import './App.css';
+import Student from './components/student/student'
+import axios from 'axios'
 
 class App extends React.Component {
+  state = {
+    students: []
+  }
+  componentDidMount(){
+    axios.get (`https://www.hatchways.io/api/assessment/students`)
+      .then (response => {
+        this.setState({students: response.data.students})
+      })
+      .catch (error => {
+        console.log("Error receiving data")
+      })
+  }
   render(){
+    console.log(this.state.students)
   return (
       <div className="App">
-        
+        {this.state.students.map(student=>{
+          return(<Student
+            key={student.id} 
+            pic={student.pic}
+            company={student.company}
+            email={student.email}
+            firstName={student.firstName}
+            lastName={student.lastName}
+            skill={student.skill}
+            grades={student.grades}
+          ></Student>)
+        })}
       </div>
     );
   }
--- src/components/student/student.js
+++ src/components/student/student.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
+
 
 class Student extends React.Component {
   render(){
       const {pic,firstName,lastName,email,company,skill,grades} = this.props;
       const name = `${firstName} ${lastName}`
-      const sum = grades.reduce((sum,num)=>sum+num,0);
-      const average = sum/grades.length;
+      
+      const sum = grades.reduce((total,num)=>total+parseInt(num),0);
+      const average = Math.round(sum/grades.length*1000)/1000;
     return (
         <div>
             <img src={`${pic}`} alt="Avatar" className="student__image"/>
