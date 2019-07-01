7cfd9d0b064478bb81bae74b3c334324289248e5
Dmitry Goronkov <gordima@yahoo.com>
2019-07-01 17:03:27 -0400

    input filter added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ import axios from 'axios'
 
 class App extends React.Component {
   state = {
-    students: []
+    students: [],
+    text:  "",
+    suggestions: []
+  }
+  onTextChanged = (e) => {
+    const value = e.target.value;
+    let suggestions = [];
+    if (value.length >= 0) {
+        const regex = new RegExp(`^${value}`, 'i');
+        suggestions = this.state.students.sort(function(a, b){
+          if(a.lastName < b.lastName) { return -1; }
+          if(a.lastName > b.lastName) { return 1; }
+          return 0;
+        })
+        let suggestionsFiltered=suggestions.filter(v => (regex.test(v.lastName))||(regex.test(v.firstName)));
+        this.setState({suggestions:suggestionsFiltered, text: value });
+        
+    }
+    
+    
   }
   componentDidMount(){
     axios.get (`https://www.hatchways.io/api/assessment/students`)
       .then (response => {
-        this.setState({students: response.data.students})
+        this.setState({students: response.data.students, suggestions: response.data.students})
       })
       .catch (error => {
         console.log("Error receiving data")
@@ -20,7 +39,8 @@ class App extends React.Component {
     console.log(this.state.students)
   return (
       <div className="App">
-        {this.state.students.map(student=>{
+        <input className="inputText" value = {this.state.text} onChange={this.onTextChanged}/>
+        {this.state.suggestions.map(student=>{
           return(<Student
             key={student.id} 
             pic={student.pic}
